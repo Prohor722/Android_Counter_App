c997dac3696d55a94f4a42ef5a899d8bcaacb36d
Prohor722 <prohor.b722@gmail.com>
2023-01-08 23:58:46 +0600

    functionality complete

--- app/src/main/java/com/example/counterapp/MainActivity.kt
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.counterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView =  findViewById(R.id.textView)
+
+    }
+    fun add(view: View){
+        val t = textView.text.toString()
+        val s = t.toInt()+1
+        val st = s.toString()
+        textView.text = st
+        Log.d("msg", st)
+    }
+
+    fun sub(view: View){
+        val t = textView.text.toString()
+        val s = t.toInt()-1
+        val st = s.toString()
+        textView.text = st
+        Log.d("msg", st)
+    }
+
+    fun reset(view: View){
+        textView.text = "0"
     }
 }
